406bdc658d425c525aca55d5deaa51bbd41ae934
YRK827 <guyelin82@gmail.com>
2025-09-24 23:19:20 -0700

    Update 00.http_basic.md

---
+++ assignments/00.http/index.mjs
@@ -0,0 +1,11 @@
+import * as http from 'node:http'
+
+const PORT = 8080
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end(`<h1>Hello World</h1>\n<a href="https://google.com">Google</a>\n`)
+})
+
+server.listen(PORT, () => console.log(`http://localhost:${PORT}/`))
